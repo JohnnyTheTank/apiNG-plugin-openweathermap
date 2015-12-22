827121d598631617c2ceb7cde5277dfe6674ae4a
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-12-22 02:13:56 +0100

    impr: changed "undefined" as default value for unused attributes in models (close #2)

--- src/aping-openweathermap-helper.js
+++ src/aping-openweathermap-helper.js
@@ -71,8 +71,8 @@ jjtApingOpenWeatherMap.service('apingOpenWeatherMapHelper', ['apingModels', 'api
     this.getRainInfoFromObject = function (_rain) {
 
         var returnObject = {
-            duration:false,
-            volume:false,
+            duration:undefined,
+            volume:undefined,
         };
 
         if(typeof _rain === "object") {
@@ -88,19 +88,19 @@ jjtApingOpenWeatherMap.service('apingOpenWeatherMapHelper', ['apingModels', 'api
     this.getWeatherInfoFromArray = function (_weatherArray) {
 
         var returnObject = {
-            code: false,
-            caption: false,
-            text : false,
-            icon_name: false,
-            icon_url: false,
+            code: undefined,
+            caption: undefined,
+            text : undefined,
+            icon_name: undefined,
+            icon_url: undefined,
         };
 
         if (_weatherArray.constructor === Array) {
-            returnObject.code = _weatherArray[0].id || false;
-            returnObject.caption = _weatherArray[0].main || false;
-            returnObject.text = _weatherArray[0].description || false;
-            returnObject.icon_name = _weatherArray[0].icon || false;
-            returnObject.icon_url = _weatherArray[0].icon ? "https://openweathermap.org/img/w/"+_weatherArray[0].icon+".png" : false;
+            returnObject.code = _weatherArray[0].id || undefined;
+            returnObject.caption = _weatherArray[0].main || undefined;
+            returnObject.text = _weatherArray[0].description || undefined;
+            returnObject.icon_name = _weatherArray[0].icon || undefined;
+            returnObject.icon_url = _weatherArray[0].icon ? "https://openweathermap.org/img/w/"+_weatherArray[0].icon+".png" : undefined;
         }
 
         return returnObject;
@@ -111,56 +111,55 @@ jjtApingOpenWeatherMap.service('apingOpenWeatherMapHelper', ['apingModels', 'api
 
         //fill _item in socialObject
         $.extend(true, weatherObject, {
-            //weather_code: false,
-            //weather_caption: false, //rain
-            //weather_text : false, //light rain
-            //weather_icon_name: false,
-            //weather_icon_url: false,
+            //weather_code: undefined,
+            //weather_caption: undefined, //rain
+            //weather_text : undefined, //light rain
+            //weather_icon_name: undefined,
+            //weather_icon_url: undefined,
 
-            temp: _item.main ? _item.main.temp : false,
-            pressure: _item.main ? _item.main.pressure : false,
-            humidity: _item.main ? _item.main.humidity : false,
-            temp_min: _item.main ? _item.main.temp_min : false,
-            temp_max: _item.main ? _item.main.temp_max : false,
-            sea_level: _item.main ? _item.main.sea_level : false,
-            grnd_level: _item.main ? _item.main.grnd_level : false,
-            wind_speed: _item.wind ? _item.wind.speed : false,
-            wind_deg: _item.wind ? _item.wind.deg : false,
-            //rain_duration: false,
-            //rain_volume: false,
-            clouds: _item.clouds ? _item.clouds.all : false,
+            temp: _item.main ? _item.main.temp : undefined,
+            pressure: _item.main ? _item.main.pressure : undefined,
+            humidity: _item.main ? _item.main.humidity : undefined,
+            temp_min: _item.main ? _item.main.temp_min : undefined,
+            temp_max: _item.main ? _item.main.temp_max : undefined,
+            sea_level: _item.main ? _item.main.sea_level : undefined,
+            grnd_level: _item.main ? _item.main.grnd_level : undefined,
+            wind_speed: _item.wind ? _item.wind.speed : undefined,
+            wind_deg: _item.wind ? _item.wind.deg : undefined,
+            //rain_duration: undefined,
+            //rain_volume: undefined,
+            clouds: _item.clouds ? _item.clouds.all : undefined,
 
             timestamp: Date.now(),
             date_time: new Date(),
 
-            sunrise_timestamp : _item.sys ? _item.sys.sunrise : false,
-            sunrise_date_time : _item.sys ? new Date( _item.sys.sunrise ) : false,
-            sunset_timestamp : _item.sys ? _item.sys.sunset : false,
-            sunset_date_time : _item.sys ? new Date( _item.sys.sunset ) : false,
+            sunrise_timestamp : _item.sys ? _item.sys.sunrise : undefined,
+            sunrise_date_time : _item.sys ? new Date( _item.sys.sunrise ) : undefined,
+            sunset_timestamp : _item.sys ? _item.sys.sunset : undefined,
+            sunset_date_time : _item.sys ? new Date( _item.sys.sunset ) : undefined,
 
-            loc_city : _item.name || false,
-            loc_city_id: _item.id || false,
-            loc_country : _item.sys ? _item.sys.country : false,
-            loc_lat : _item.coord ? _item.coord.lat : false,
-            loc_lng : _item.coord ? _item.coord.lon : false,
+            loc_city : _item.name || undefined,
+            loc_city_id: _item.id || undefined,
+            loc_country : _item.sys ? _item.sys.country : undefined,
+            loc_lat : _item.coord ? _item.coord.lat : undefined,
+            loc_lng : _item.coord ? _item.coord.lon : undefined,
             //loc_zip : false,
         });
 
         if(_item.rain) {
             var tempRainObject = this.getRainInfoFromObject(_item.rain);
-            weatherObject.rain_duration = tempRainObject.duration || false;
-            weatherObject.rain_volume = tempRainObject.volume || false;
+            weatherObject.rain_duration = tempRainObject.duration || undefined;
+            weatherObject.rain_volume = tempRainObject.volume || undefined;
         }
 
         if(_item.weather) {
             var tempWeatherObject = this.getWeatherInfoFromArray(_item.weather);
-            weatherObject.weather_code = tempWeatherObject.code || false;
-            weatherObject.weather_caption = tempWeatherObject.caption || false;
-            weatherObject.weather_text = tempWeatherObject.text || false;
-            weatherObject.weather_icon_name = tempWeatherObject.icon_name || false;
-            weatherObject.weather_icon_url = tempWeatherObject.icon_url || false;
+            weatherObject.weather_code = tempWeatherObject.code || undefined;
+            weatherObject.weather_caption = tempWeatherObject.caption || undefined;
+            weatherObject.weather_text = tempWeatherObject.text || undefined;
+            weatherObject.weather_icon_name = tempWeatherObject.icon_name || undefined;
+            weatherObject.weather_icon_url = tempWeatherObject.icon_url || undefined;
         }
-
 
         return weatherObject;
     };
